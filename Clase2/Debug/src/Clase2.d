204a39ab592be8ac466e6208aa1d20bcc6bf1e1c
alumno <alumno@LAB00PC00>
2019-08-22 21:10:33 -0300

    Agregando mi primer error con los punteros

---
+++ Clase2/Debug/src/Clase2.d
@@ -0,0 +1,1 @@
+src/Clase2.o: ../src/Clase2.c
---
+++ Clase2/src/Clase2.c
@@ -0,0 +1,27 @@
+/*
+ ============================================================================
+ Name        : Clase2.c
+ Author      : joariel93
+ Version     :
+ Copyright   : GLP 3
+ Description : Clase 2 primeros algoritmos
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int calcularMitad(int*);
+int main(void) {
+	int valor;
+valor = calcularMitad(&valor);
+
+printf("La mitad del numero  ingresado es: %d",valor);
+int calcularMitad(int* vla){
+	int numero;
+	printf("Ingrese un numero: ");
+	scanf("%d",&numero);
+	*vla=numero*0.5;
+	return 0;
+	}
+
+}
