7bc03e83f5b7da62d0ea79175694b437c7835709
alumno <alumno@lab6pc00>
2019-08-28 19:09:36 -0300

    Empieza la clase 3

---
+++ Clase-3/src/Clase-3.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Clase-3.c
+ Author      : joariel93
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
